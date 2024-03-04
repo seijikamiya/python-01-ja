d89f2524f848a6076703b8e09a372a7ed8650e50
Seiji Kamiya <10seiji18@gmail.com>
2024-03-04 18:23:35 +0900

    Modify count_vowels.py for challenge

--- 02_creating_python_files_and_control_flow/03_challenge/count_vowels.py
+++ 02_creating_python_files_and_control_flow/03_challenge/count_vowels.py
@@ -1,1 +1,11 @@
 # ここにコードを書いてください
+word = input('Enter a string:')
+vowels = "aeiou"
+number_of_vowels = 0
+
+for letter in word.lower():
+    for vowel in vowels:
+        if letter == vowel:
+            number_of_vowels += 1
+
+print(f"Number of vowels:{number_of_vowels}")
