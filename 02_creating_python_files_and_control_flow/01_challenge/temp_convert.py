1509a3515e455475ff27aa550de8b46047127404
Seiji Kamiya <10seiji18@gmail.com>
2024-03-04 18:22:48 +0900

    Modify temp_convert.py for challenge

--- 02_creating_python_files_and_control_flow/01_challenge/temp_convert.py
+++ 02_creating_python_files_and_control_flow/01_challenge/temp_convert.py
@@ -1,9 +1,14 @@
 def convert():
     # ここにコードを書いてください
     # temp変数を編集し、ユーザー入力として温度を受け取ります。整数に変換することを忘れないでください
-    temp = 0
+    temp = int(input('温度を入力して下さい'))
+    unit = input('温度の単位をfかcで入力して下さい')
+
+    if unit == "f":
+        temp = (temp - 32) * 5/9
+    elif unit == "c":
+        temp = temp * 9/5 + 32
 
     return temp
-
 
 convert()
