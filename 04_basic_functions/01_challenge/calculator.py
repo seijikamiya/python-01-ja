e3621a2bb363ed4b11d9a83a28e26294d8fdb6a7
Seiji Kamiya <10seiji18@gmail.com>
2024-03-05 22:09:09 +0900

    Modify calculator for challenge

--- 04_basic_functions/01_challenge/calculator.py
+++ 04_basic_functions/01_challenge/calculator.py
@@ -1,14 +1,29 @@
 def add(a, b):
-    return 0
+    if type(a) in [float, int] and type(b) in [float, int]:
+        return a+b
+    else:
+        return "bad input"
 
 
 def subtract(a, b):
-    return 0
+    if type(a) in [float, int] and type(b) in [float, int]:
+        return a-b
+    else:
+        return "bad input"
 
 
 def multiply(a, b):
-    return 0
+    if type(a) in [float, int] and type(b) in [float, int]:
+        return a*b
+    else:
+        return "bad input"
 
 
 def divide(a, b):
-    return 0
+    if type(a) in [float, int] and type(b) in [float, int]:
+        if b != 0:
+            return a/b
+        else:
+            return "Cannot divide by zero"    
+    else:
+        return "bad input"
