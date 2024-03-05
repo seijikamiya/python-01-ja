436651db421afd3ebb90aef469137aebd1d7fa9f
Seiji Kamiya <10seiji18@gmail.com>
2024-03-05 22:30:11 +0900

    Modify main.py for 04_basic_functions/03_challenge

--- 04_basic_functions/03_challenge/main.py
+++ 04_basic_functions/03_challenge/main.py
@@ -1,6 +1,12 @@
 def unique_substrings(input_string):
     # 「pass」を削除して、ここにコードを書いてください
-    pass
+    unique_substrings_list = []
+    for i in range(len(input_string)):
+        for j in range(i+1, len(input_string)+1):
+            substring = input_string[i:j]
+            if substring not in unique_substrings_list:
+                unique_substrings_list.append(substring)
+    return unique_substrings_list
 
 
 input_string = "banana"
