df9736f13a585ff12d5417380d2d4e62f9706527
Seiji Kamiya <10seiji18@gmail.com>
2024-03-05 14:10:31 +0900

    Modify ls_command.py for challenge

--- 03_basic_data_structures_and_file_handling/03_challenge/ls_command.py
+++ 03_basic_data_structures_and_file_handling/03_challenge/ls_command.py
@@ -6,10 +6,25 @@ def ls_command(directory):
     # 無効なディレクトリパスについて考える必要はありません
 
     # 「pass」を削除して、ここにコードを書いてください
-    pass
+    extension_dict = {}
+    file_list = os.listdir(directory)
+    for file in file_list:
+        extension = file.split(".")[-1]
+        if extension in extension_dict:
+            extension_dict[extension] += 1
+        else:
+            extension_dict[extension] = 1
 
+    print("Summary in alphabetical order:")
+    for key, value in sorted(extension_dict.items()):
+        if value > 1:
+            print(f".{key}:{value} files")
+        else:
+            print(f".{key}:{value} file")
 
 if __name__ == "__main__":
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    os.chdir(current_dir)
     directory_path = input("Enter directory path to organize files: ")
 
     if not os.path.isdir(directory_path):
