b34bd336213b3409428bed0168d3a7636f1e9f4d
Seiji Kamiya <10seiji18@gmail.com>
2024-03-05 13:39:46 +0900

    Modify counter.py for challenge

--- 03_basic_data_structures_and_file_handling/01_challenge/counter.py
+++ 03_basic_data_structures_and_file_handling/01_challenge/counter.py
@@ -14,6 +14,14 @@ def counter():
     ]
 
     # ここにコードを書いてください
+    for fruit in fruits:
+        if fruit in occurrences:
+            occurrences[fruit] += 1
+        else:
+            occurrences[fruit] = 1
+
+    for key, value in occurrences.items():
+        print(f'{key}: {value}')        
 
     return occurrences
 
