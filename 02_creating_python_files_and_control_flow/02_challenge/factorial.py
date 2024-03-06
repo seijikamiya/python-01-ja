6f95bbf58e936d69132a973fd017da86d46dcecd
Seiji Kamiya <10seiji18@gmail.com>
2024-03-06 14:32:47 +0900

    modify 02_creating_python_files_and_control_flow/02_challenge/factorial.py

--- 02_creating_python_files_and_control_flow/02_challenge/factorial.py
+++ 02_creating_python_files_and_control_flow/02_challenge/factorial.py
@@ -1,10 +1,14 @@
 def compute_factorial():
     # ここにコードを書いてください
     # number変数を編集し、ユーザー入力として正の整数を受け取ります。整数に変換することを忘れないでください
-    number = 0
-
-    # result変数を編集し、最終的な計算結果を保存します
-    result = 0
+    number = int(input('Please input an integer: '))
+    if number < 0:
+        print('pleases input a positive value')
+    else:
+        result = 1
+        if number >= 2:
+            for i in range(1, number+1):
+                result *= i
 
     return result
 
