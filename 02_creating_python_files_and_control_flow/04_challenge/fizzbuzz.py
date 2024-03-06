c623567d29fd74cf43a67f2c01ffe05a424ca6a3
Seiji Kamiya <10seiji18@gmail.com>
2024-03-06 14:41:50 +0900

    Modify fizzbuzz.py for challenge

--- 02_creating_python_files_and_control_flow/04_challenge/fizzbuzz.py
+++ 02_creating_python_files_and_control_flow/04_challenge/fizzbuzz.py
@@ -1,1 +1,10 @@
 # ここにコードを書いてください
+for num in range(1, 101):
+    if num % 3 == 0 and num % 5 != 0:
+        print('Fizz')
+    elif num % 3 != 0 and num % 5 == 0:
+        print('Buzz')
+    elif num % 3 == 0 and num % 5 == 0:
+        print('FizzBuzz')
+    else:
+        print(num)
